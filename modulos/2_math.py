201efe5c03abc125833050319be40ad087fd2af9
Maicon Souza <maicon_souza09@hotmail.com>
2024-08-15 17:27:14 -0300

    15-08-24 17:24

---
+++ modulos/2_math.py
@@ -0,0 +1,21 @@
+import math
+
+print(math.pi)
+print(f"{math.pi:.2f}")
+
+print(math.e)
+print(f"{math.e:.2f}")
+
+num = 11.4
+print(math.ceil(num))
+print(math.floor(num))
+
+print(math.factorial(3))
+
+print(math.pow(5, 5))
+
+print(math.sqrt(49))
+
+print(math.gcd(20, 70))
+
+print(math.log(10))
---
+++ modulos/3_statistic.py
@@ -0,0 +1,10 @@
+import statistics
+
+print(statistics.mean([2, 8, 10, 9, 8, 10, 7, 10, 1, 10]))
+
+print(statistics.median([1, 2, 4, 8, 9]))
+print(statistics.median([1, 2, 3, 7, 8, 9]))
+
+print(statistics.mode([2, 8, 9, 2, 23, 1, 23, 8, 21, 23, 99, 9, 8, 5, 23]))
+
+print(statistics.stdev([1, 1.5, 2, 2.5, 2.75, 3.25, 4.75]))
